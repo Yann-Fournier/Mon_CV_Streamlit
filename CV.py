a1a6e8fb0e6b1e0003ca85dfea12d1d3c48859dd
Yann-Fournier <y.fourni@icloud.com>
2023-10-25 14:52:17 +0200

    add img

--- CV.py
+++ CV.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+path = "C:/Users/yfour/OneDrive/Documents/Mon_CV_Streamlit"
+img_cv = path + "\image\CV.pdf"
+img_profil = path + "\image\Profil.pdf"
 
 st.write("Voici mon CV")
-image = Image.open('C:/Users/yfour/OneDrive/Documents/papier/CV.jpg')
+image = Image.open(img_cv)
 st.image(image, caption="Mon CV", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
+image = Image.open(img_profil)
+st.image(image, caption="Mon CV", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
