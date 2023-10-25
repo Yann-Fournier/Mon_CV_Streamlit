1c39555c284484c482f94584010b8c94bf354e99
Yann-Fournier <y.fourni@icloud.com>
2023-10-25 15:02:35 +0200

    add img

--- CV.py
+++ CV.py
@@ -3,12 +3,9 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 
-path = "C:/Users/yfour/OneDrive/Documents/Mon_CV_Streamlit"
-img_cv = path + "\image\CV.pdf"
-img_profil = path + "\image\Profil.pdf"
 
 st.write("Voici mon CV")
-image = Image.open(img_cv)
+image = Image.open('./image/CV.jpg')
 st.image(image, caption="Mon CV", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-image = Image.open(img_profil)
-st.image(image, caption="Mon CV", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+image2 = Image.open("image/Profil.png")
+st.image(image2, caption="Mon CV", width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
